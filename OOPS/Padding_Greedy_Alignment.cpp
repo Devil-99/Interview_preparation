fc0778819288561b8b8bd1c90a1f15bbdbd077fe
Devil-99 <souvikmaji1999@gmail.com>
2022-09-27 23:33:36 +0530

    Added new file Fundamentals

---
+++ OOPS/Padding_Greedy_Alignment.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Data{
+    char a;
+    char c;
+    short int b;
+    int d;
+};
+
+int main() {
+    Data obj;
+    cout<<sizeof(obj)<<endl;
+    return 0;
+}
