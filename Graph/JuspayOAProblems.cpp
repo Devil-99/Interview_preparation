e87493fc5788ab4687c2800637904a607e967e21
Devil-99 <souvikmaji1999@gmail.com>
2022-10-12 11:16:38 +0530

    Juspay OA Problems

--- Graph/JuspayOAProblems.cpp
+++ Graph/JuspayOAProblems.cpp
@@ -43,6 +43,24 @@ int maximumWeightedNode(vector<int>&edges){
     return -1;
 }
 
+void shortPath(vector<int>adj[], int c1, int n, vector<int>&dist){
+    queue<int>q;
+    q.push(c1);
+    dist[c1]=0;
+
+    while(!q.empty()){
+        int temp=q.front();
+        q.pop();
+
+        for(auto &it:adj[temp]){
+            if(dist[it]>dist[temp]+1){
+                dist[it]=dist[temp]+1;
+                q.push(it);
+            }
+        }
+    }
+}
+
 int main()
 {
     int n=23;
@@ -56,7 +74,30 @@ int main()
     }
     cout<<"Longest cycle is "<<mx<<endl;
 
-    cout<<"Maximum Weighted Node is "<<maximumWeightedNode(edges);
+    cout<<"Maximum Weighted Node is "<<maximumWeightedNode(edges)<<endl;
+
+    int c1=14,c2=16;
+    vector<int> adj[n];
+    for(int i=0 ; i<n ; i++){
+        if(edges[i] == -1) continue;
+        adj[i].push_back(edges[i]);
+    }
+    vector<int>v1(n,INT_MAX);
+    vector<int>v2(n,INT_MAX);
+
+    shortPath(adj, c1, n, v1);
+    shortPath(adj, c2, n, v2);
+
+    int mn = INT_MAX, node = -1;
+    for(int i=0 ; i<n ; i++){
+        if(v1[i] == INT_MAX || v2[i] == INT_MAX)
+            continue;
+        if(v1[i] + v2[i] < mn){
+            mn = v1[i] + v2[i];
+            node = i;
+        }
+    }
+    cout <<"Nearest node from "<<c1<<" and "<<c2<<" is "<< node << endl;
 
 return 0;
 }
